bc645cdc9cac5674e63bf2b03f12a0fa91ca0833
iii <iii@userde-MacBook-6.local>
2017-06-21 11:28:52 +0800

    v1

--- MyApp05/ViewController.swift
+++ MyApp05/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var svalue: UISwitch!
+    
+    @IBOutlet weak var stepValue: UIStepper!
 
+    @IBOutlet weak var myBtn: UIButton!
+    
+    private var isOn:Bool?
+    
+    @IBAction func switchStatus(_ sender: Any) {
+         print(svalue.isOn)
+    }
+    
+    
+    @IBAction func changeValue(_ sender: Any) {
+         print(stepValue.value)
+    }
+    
+    @IBAction func clickButton(_ sender: Any) {
+        myBtn.isSelected = !myBtn.isSelected
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    stepValue.maximumValue = 10
+    stepValue.minimumValue = 0
+    stepValue.value = 5
+    stepValue.stepValue = 0.5
+    
+    myBtn.setImage(UIImage(named:"sw_on"), for: UIControlState.normal)
+    myBtn.setImage(UIImage(named:"sw_off"), for: UIControlState.selected)
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
